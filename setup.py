275e41b82013bd8d0dfaedfc2bbca9712d903556
kang-team <lemon13@hanmail.net>
2025-05-19 23:20:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 long_description = fh.read()
 
 setuptools.setup(
-name="simple_scheduler", # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
+name="kang_pip_lib", # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
 version="0.0.1",
-author="Donghyun Kwak",
-author_email="donghyun.kwak@naver.com",
-description="A simplest scheduler for automated data preprocessing and nlu model training.",
+author= "kang",
+author_email="firstreal1@naver.com",
+description="A kang pip lib",
 long_description=long_description,
 long_description_content_type="text/markdown",
-url="https://github.com/dhkwak/simple_scheduler",
+url="https://github.com/kang-team/git_pip_install.git",
 packages=setuptools.find_packages(),
 classifiers=[
 "Programming Language :: Python :: 3",
@@ -25,8 +25,3 @@ python_requires='>=3.6',
 install_requires=requirements,
 )
 
-https://packaging.python.org/tutorials/packaging-projects/
-
-https://docs.python.org/3.6/distutils/setupscript.html
-
-https://www.jetbrains.com/help/pycharm/creating-and-running-setup-py.html
